8b2f7a98738e7bf08e5bc0c068f6a29630209228
natalie <natalie.soltis@outlook.com>
2023-01-30 20:25:06 -0500

    what if this works

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,10 @@
 var gulp = require("gulp");
 var deploy = require("gulp-gh-pages");
+var run = require("gulp-run");
 
 /**
  * Push build to gh-pages
  */
-gulp.task("deploy", ["build"], function() {
+gulp.task("deploy", function() {
   return gulp.src("./build/**/*").pipe(deploy());
-});
-
-gulp.task("build", function() {
-  return run("npm run build").exec();
 });
