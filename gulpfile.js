eaa87d40b461a42466427b523d5dcb3ba0e39d11
natalie <natalie.soltis@outlook.com>
2023-01-30 19:52:55 -0500

    yeah 2

--- gulpfile.js
+++ gulpfile.js
@@ -5,5 +5,5 @@ var deploy = require("gulp-gh-pages");
  * Push build to gh-pages
  */
 gulp.task("deploy", function() {
-  return gulp.src("./src/**/*").pipe(deploy());
+  return gulp.src("./build/**/*").pipe(deploy());
 });
