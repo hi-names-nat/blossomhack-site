0cae76d70e49767c6dfded7fd9e4fa7dfa3b1a51
llukehaskell <lukehaskell@gmail.com>
2023-07-20 13:27:56 -0400

    added event date & luke's website

--- src/components/Schedule/Schedule.jsx
+++ src/components/Schedule/Schedule.jsx
@@ -4,7 +4,8 @@ import './Schedule.css'
 export default function Schedule() {
   return (
     <table>
-      <tr><th>April 2024: schedule TBA!</th></tr>
+      <p>BlossomHack 2024 runs through April 6th-7th. Schedule TBA!</p>
+      {/* <tr><th>Schedule TBA!</th></tr> */}
     </table>
   )
 }
