a7bb1396e1ee5e6e7bfdb454a36ac197b1f50daa
llukehaskell <lukehaskell@gmail.com>
2023-08-01 12:54:12 -0400

    fixed section headers being invisible lol

--- src/components/Front/Front.jsx
+++ src/components/Front/Front.jsx
@@ -11,7 +11,7 @@ export default function front() {
         </div>
         <div className="HeroText">
           <h1>The Biggest Hackathon at Ohio University</h1>
-          <h2>April 6th-7th, 2024</h2>
+          <h2><div className="date">April 6th-7th, 2024</div></h2>
           <div className="logobutton">
             <a href="https://l5kysdlzy94.typeform.com/to/IOsDKBh4" target="_blank">
               <button role="button" className="btn"><h1 className="hbtn">Register →</h1></button>
