68df2f5051a38c701c023e57ca49db89c8fc8f52
MonadoBarrage <chenkatti@tutanota.com>
2023-08-06 17:35:19 -0400

    Modified SponsorPage

--- src/components/SponsorPage/SponsorPage.jsx
+++ src/components/SponsorPage/SponsorPage.jsx
@@ -1,8 +1,48 @@
+import React from 'react';
 import SponsorPageNavbar from './SponsorPageNavbar';
+import Footer from '../Footer/Footer';
 import { Image } from "semantic-ui-react";
+import { Outlet } from "react-router-dom";
+import SponsorFront from './SponsorFront';
+import Page from '../Page/Page';
+
 export default function SponsorPage() {
-  
-  
+
+  const [sections, _] = React.useState([
+    {
+      sectionName: "What is BlossomHack?",
+      subtitle: "",
+    },
+    {
+      sectionName: "Our Events",
+      subtitle: "",
+    },
+    {
+      sectionName: "FAQ",
+      subtitle: "",
+    },
+    {
+      sectionName: "Sponsors",
+      subtitle: "",
+    },
+    {
+      sectionName: "Our Team",
+      subtitle: "",
+    },
+    {
+      sectionName: "Hosted By",
+      subtitle: "",
+    },
+    {
+      sectionName: "Get in touch",
+      subtitle: "",
+    },
+    {
+      sectionName: "Schedule",
+      subtitle: "",
+    },
+  ]);
+
   function importAll(r) {
     let images = {};
     r.keys().map((item, index) => {
@@ -10,22 +50,25 @@ export default function SponsorPage() {
     });
     return images;
   }
-  
+
   const images = importAll(
     require.context("../../images/sponsors", false, /\.(png|jpe?g|svg)$/)
   );
-  
+
   return (
     <div id="sponsor-page">
-      <SponsorPageNavbar/>
+      <SponsorPageNavbar />
       {/* <img src="https://placekitten.com/g/200/200"/> */}
       {/* <img src = {require('../../images/sponsors/ai_futures.png')}/> */}
-      <Image src={images["ai_futures.png"]}/>
-        <p>
-          This is a test
-          </p>
+      <SponsorFront />
+      <Page sections={sections} />
+      <hr />
+      <Footer />
+      <div id="detail">
+        <Outlet />
       </div>
-    
+    </div>
+
   );
-  
+
 }
